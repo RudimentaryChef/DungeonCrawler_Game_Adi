82d999aa68c437b9faad6edcb16d70a64d532c0d
Rishi1204 <rishinopany@gmail.com>
2023-09-18 22:43:29 -0400

    Create MainActivity.java

---
+++ app/src/main/java/com/example/cs2340c_team41/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs2340c_team41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
