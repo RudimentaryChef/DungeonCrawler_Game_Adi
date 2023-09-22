bc51e4431a5cb6c198455a89e135efa78f8a7ff5
RudimentaryChef <akrish6@gatech.edu>
2023-09-22 13:21:22 -0400

    fixes start button and button colors

--- app/src/main/java/com/example/cs2340c_team41/MainActivity.java
+++ app/src/main/java/com/example/cs2340c_team41/MainActivity.java
@@ -19,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+        startApplicationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent()
+                System.out.println("hi");
+            }
+        });
 
     }
 }
